88e719cfb4702e047521a613b2e51da49585dcbc
Iaroslav Popov <ip13@sanger.ac.uk>
2023-02-15 15:33:20 +0000

    seq_count implemented
    A command line utility, given the path to a FASTQ file and a flag requesting the sequence count, prints to STDOUT the correct number of sequences in that file

--- bioinf-toolkit.py
+++ bioinf-toolkit.py
@@ -4,10 +4,15 @@ from src.fastq import Fastq
 
 def main():
     parser = argparse.ArgumentParser(description='Sanger Bioinformatics Toolkit')
-    parser.add_argument('-f', '--fastq', help='path to fastq file', required=True)
+    parser.add_argument('-f', '--fastq', help='path to fastq file', required=True, type=str)
+    parser.add_argument('--seq_count', help='prints the sequence count', action='store_true')
     args = parser.parse_args()
 
     fastq = Fastq(path=args.fastq)
+
+    if args.seq_count:
+        n = fastq.count_seqs()
+        print(n)
 
 
 if __name__ == '__main__':
--- src/fastq.py
+++ src/fastq.py
@@ -13,3 +13,18 @@ class Fastq:
 
         if not self.path.endswith(('.fastq', 'fq')):
             print(f'{self.path} does not seem to be a fastq file. Please check the path')
+
+    def count_seqs(self) -> int:
+        """ counts number of sequences in file """
+        lines = 0
+        with open(self.path) as f:
+            for line in f:
+                lines += 1
+
+        seqs = lines / 4
+        if seqs.is_integer():
+            seqs = int(seqs)
+        else:
+            raise ValueError('Number of lines is not multiple by 4')
+
+        return seqs
